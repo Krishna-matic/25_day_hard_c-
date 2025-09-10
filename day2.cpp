d0a6455e1e601b25359def62fb8dada5fed3e9b1
Krishna-Matic <kl7112180@gmail.com>
2025-09-10 19:42:30 +0530

    Added Day 2 code for 25-day C++ challenge

---
+++ day2.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     To check if the year is leap year or not.
+//     int year;
+//     cout<<"Enter the year to check: ";
+//     cin>>year;
+//     if(year%4==0){
+//         cout<<year<<" is a leap year.";
+//     }else{
+//         cout<<year<<" is not a leap year.";
+//     }
+//     Take two numbers and print their sum
+//     int a,b,sum;
+//     cout<<"Enter your first number: ";
+//     cin>>a;
+//     cout<<"Enter your second number: ";
+//     cin>>b;
+//     cout<<"The sum of both the numbers is: "<<(sum=a+b);
+//     Take a number as input and print the multiplication table for it.
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     for(int i=1;i<=10;i++){
+//         cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+//     }
+// }
