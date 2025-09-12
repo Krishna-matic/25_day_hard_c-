2ef55a15a8a65bc70db514b2d790a7255498f0da
Krishna-Matic <kl7112180@gmail.com>
2025-09-12 19:28:40 +0530

    Added Day 4 Code for 25-day C++ challenge

---
+++ day4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int r;
+    // cout<<"Enter the radius of circle: ";
+    // cin>>r;
+    // cout<<"Area of the circle is: "<<(3.14*r*r)<<" m square.";
+    // float b,h;
+    // cout<<"Enter your triangle height: ";
+    // cin>>h;
+    // cout<<"Enter your triangle base: ";
+    // cin>>b;
+    // cout<<"Area of the triangle is: "<<(0.5*b*h)<<" m sqaure.";
+    // float b,l;
+    // cout<<"Breadth of the rectangle: ";
+    // cin>>b;
+    // cout<<"Lenght of the rectangle: ";
+    // cin>>l;
+    // cout<<"Area of the rectangle is: "<<l*b<<" m square.";
+    // float r,h;
+    // cout<<"Enter your height of cylinder: ";
+    // cin>>h;
+    // cout<<"Enter your radius of cylinder: ";
+    // cin>>r;
+    // cout<<"Volume of your cylinder is: "<<3.14*r*r*h<<" m cube.";
+    float l,w,h;
+    cout<<"Height of the pyramid: ";
+    cin>>h;
+    cout<<"Widht of the pyramid: ";
+    cin>>w;
+    cout<<"Length of the pyramid: ";
+    cin>>l;
+    cout<<"Volume of the pyramid is: "<<(l*w*h)/3<<" m cube.";
+}
