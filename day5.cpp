40559f5196ad135d47bf39f5b78ecd8257f0334e
Krishna-Matic <kl7112180@gmail.com>
2025-09-13 20:26:21 +0530

    Added Day 5 Code for 25-day C++ challenge

---
+++ day5.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int a,b;
+    // cout<<"Enter your first number: ";
+    // cin>>a;
+    // cout<<"Enter your second number: ";
+    // cin>>b;
+    // cout<<"Addition of both the numbers is: "<<a+b<<".";
+    //Fibonnaci series
+    // int n,a=0,b=1,next;
+    // cout<<"Enter the number: ";
+    // cin>>n;
+    // cout<<"Fibonnaci series: ";
+    // for(int i=1;i<=n;i++){
+    //     cout<<" "<<a;
+    //     next=a+b;
+    //     a=b;
+    //     b=next;  
+    // }
+    //Factorial of a number.
+    // int n,fact=1;
+    // cout<<"Enter you number: ";
+    // cin>>n;
+    // for(int i=1;i<=n;i++){
+    //     fact=fact*i;
+    // }
+    // cout<<"Factorial of a number is: "<<fact<<".";
+    // average of natural numebers till n.
+    // float n,sum=0;
+    // float avg=0;
+    // cout<<"Enter your number: ";
+    // cin>>n;
+    // for(float i=1;i<=n;i++){
+    //     sum = i+sum;
+    //     avg = sum/n;
+    // }
+    // cout<<"Average of all the natural number till "<<n<<" is: "<<avg;
+    //factors of a number.
+    // int n;
+    // cout<<"Enter a number: ";
+    // cin>>n;
+    // cout<<"Factors of this number is: ";
+    // for(int i=1;i<=n;i++){
+    //     if(n%i==0){
+    //         cout<<i<<" ";
+    //     }
+    // }
+}
