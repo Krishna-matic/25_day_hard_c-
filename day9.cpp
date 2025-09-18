8224905be7b2e008f0557d3adcce38ed1ca9dd95
Krishna-Matic <kl7112180@gmail.com>
2025-09-18 22:06:08 +0530

    Added Day 9 Code for 25-day C++ challenge

---
+++ day9.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+//Define function to print the maximum and the minimum number respectively among three numbers entered by the user.
+// void greatest(int a,int b,int c){
+//     if(a>b&&a>c){
+//         cout<<"Greatest of all number is: "<<a<<endl;
+//     }else if(b>a&&b>c){
+//         cout<<"Greatest of all number is: "<<b<<endl;
+//     }else{
+//         cout<<"Greates of all number is: "<<c<<endl;
+//     }
+// }
+// void smallest(int x,int y, int z){
+//     if(x<y&&x<z){
+//         cout<<"Smallest of all number is: "<<x<<endl;
+//     }else if(y<x&&y<z){
+//         cout<<"Smallest of all number is: "<<y<<endl;
+//     }else{
+//         cout<<"Smallest of all number is: "<<z<<endl;
+//     }
+// }
+// int main(){
+//     int p,q,r;
+//     cout<<"Enter the three numbers: "<<endl;
+//     cin>>p>>q>>r;
+//     smallest(p,q,r);
+//     greatest(p,q,r);
+//     return 0;
+// }
+//Define a function to find out whether a given number is even or odd.
+// void check(int a){
+//     if(a%2==0){
+//         cout<<a<<" is even.";
+//     }else{
+//         cout<<a<<" is odd.";
+//     }
+// }
+// int main(){
+//     int x;
+//     cout<<"Enter your number to check: ";
+//     cin>>x;
+//     check(x);
+//     return 0;
+// }
+//A person is eligible to vote if his/her age is greater than or equal to 18. Define a function to find out if he/she is eligible to vote
+// void eligible(int a){
+//     if(a>=18){
+//         cout<<"Eligibile to vote.";
+//     }else{
+//         cout<<"Not eligible.";
+//     }
+// }
+// int main(){
+//     int x;
+//     cout<<"Enter the age of person: ";
+//     cin>>x;
+//     eligible(x);
+//     return 0;
+// }
+//Write a program to print the sum of two numbers entered by user by defining your own function.
+// void sum(int a, int b){
+//     cout<<"The sum of both the number is: "<<a+b;
+// }
+// int main(){
+//     int x,y;
+//     cout<<"Enter both the numbers: "<<endl;
+//     cin>>x>>y;
+//     sum(x,y);
+//     return 0;
+// }
